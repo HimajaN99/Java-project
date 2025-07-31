7e36291b3112eb53850bcb57983abdcd46636ca5
HimajaN99 <himajasai.n@gmail.com>
2025-07-31 13:26:28 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Java build!");
+        System.out.println("This is during jenkins practise -- poll scm !!");
     }
 }
